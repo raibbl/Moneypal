2f9d68d2e4f07f1c034b1aedc469b9702dbd09e0
raibbl <raibbl1997@gmail.com>
2019-07-31 16:06:20 -0400

    Connected list somewhat

--- App.js
+++ App.js
@@ -40,8 +40,8 @@ class AddScreen extends React.Component {
 
     //  AsyncStorage.setItem('i',i );
 
-    
-    this.state = { count:0,fontLoaded: false, text: '', name: '', amount: '', budget: '', date: '', year: '', month: '', day: '', longitude: 0, latitude: 0, pickerValue: '' };
+
+    this.state = { trans: '', fontLoaded: false, text: '', name: '', amount: '', budget: '', date: '', year: '', month: '', day: '', longitude: 0, latitude: 0, pickerValue: '' };
   }
 
 
@@ -93,7 +93,7 @@ class AddScreen extends React.Component {
 
     //get everything from state
 
-    let { year, month, day, name, amount, budget, longitude, latitude,count } = this.state;
+    let { year, month, day, name, amount, budget, longitude, latitude, count } = this.state;
 
     //  console.log(name);
     trans = {
@@ -112,6 +112,10 @@ class AddScreen extends React.Component {
 
     newTrans.push(trans);
 
+    this.setState({ trans: newTrans });
+    //pass newTrans to new home
+
+    //this.props.navigation.navigate('Home', { newTrans})
     await AsyncStorage.setItem('ex', JSON.stringify(newTrans))
       .then(() => {
         console.log('It was saved successfully')
@@ -137,14 +141,15 @@ class AddScreen extends React.Component {
 
 
   render() {
-
+    let x = this.state.trans;
+   // alert(x);
     return (
 
 
       <PaperProvider>
         <React.Fragment>
           <Appbar>
-            <Appbar.BackAction onPress={() => this.props.navigation.navigate('Home')} />
+            <Appbar.BackAction onPress={() => this.props.navigation.navigate('Home', { x })} />
             <Appbar.Content title='Go Back' />
           </Appbar>
           <View style={{ flex: 1, alignItems: "center", backgroundColor: 'white' }}>
@@ -254,20 +259,61 @@ class HomeScreen extends React.Component {
       visible: false,
     }
   }
+
+
+  constructor(props) {
+    super(props);
+
+    this.formatData();
+    let x = this.props.navigation.x
+    //alert(x);
+    this.state = { items: [], name: [] }
+
+
+
+  }
+
+  componentDidMount() {
+
+  }
+  componentDidUpdate() {
+    //this.formatData()
+
+  }
+  //to return the transactions in keys
+
+
+  clearAsyncStorage = async () => {
+    AsyncStorage.clear();
+  }
+
+
   formatData = async () => {
-    let booga='';
-    
-       //const keys = await AsyncStorage.getAllKeys()
-        booga = await AsyncStorage.getItem('ex')
-        //const x=JSON.parse(booga);
-       // let k=x[1].name
-       // console.log( k);
-       let y = JSON.parse(booga);
-       
-       this.setState({items:y[0]});
-       //return items
-    
- 
+    let booga = '';
+
+    //const keys = await AsyncStorage.getAllKeys()
+    booga = await AsyncStorage.getItem('ex')
+    //const x=JSON.parse(booga);
+    // let k=x[1].name
+    // console.log( k);
+    let y = JSON.parse(booga);
+
+   // var myStringArray = ["Hello", "World"];
+    let arrayLength = y.length;
+    let temparray = []
+    for (var i = 0; i < arrayLength; i++) {
+        temparray.push(y[i]);
+      
+      //Do something
+    }
+    this.setState({ name: temparray })
+
+
+
+
+    //return items
+
+
     // return booga;
 
 
@@ -275,26 +321,24 @@ class HomeScreen extends React.Component {
 
 
   }
-  constructor(props) {
-    super(props);
 
-    this.state ={items:[]}
-    
-  }
-
-  //to return the transactions in keys
-
-  async componentDidMount() {
+  parseme() {
     this.formatData();
+    let { items } = this.state;
+    //let mama =JSON.parse(items);
+    return items;
   }
 
-  clearAsyncStorage = async() => {
-    AsyncStorage.clear();
+  
+rendernameoftrans () {
+  let {name} = this.state;
+  //let x =Array.from(name);
+  return name.map((item) => {
+      return (
+        <Text>{item.name}</Text>
+      );
+  });
 }
-  
-  
-
-    
   render() {
     const data = {
       labels: ['January', 'February', 'March', 'April', 'May', 'June'],
@@ -311,47 +355,53 @@ class HomeScreen extends React.Component {
       // strokeWidth: 2 // optional, default 3
     }
     const screenWidth = Dimensions.get('window').width
-    
+
     //console.log(items);
-   //let item1 = items[0]['name'];
+    //let item1 = items[0]['name'];
     //alert(item1);
     //let booga1 = 
-    //formatData()
-    let {items} = this.state;
+    // this.formatData()
+    let { name } = this.state
+    //let name = items[0].toString()
+    //let atest = this.parseme().toString();
+    //alert(items);
+
     return (
-     
+
       //<PaperProvider>
 
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <View style={{ flex: 1 }} />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <View style={{ flex: 1 }} />
 
-          <BarChart
-            data={data}
-            width={screenWidth}
-            height={220}
-            chartConfig={chartConfig}
-            bezier
-          />
+        <BarChart
+          data={data}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+          bezier
+        //<Text>Name:{items.name} Amount:{items.amount} Month:{items.month} Day:{items.day}</Text>
+        />
 
 
-          <View style={{ flex: 1 }} />
-          
-          <Button onPress={() => this.props.navigation.navigate('Add')} >
-            Add expense
+        <View style={{ flex: 1 }} />
+
+        <Button onPress={() => this.props.navigation.navigate('Add')} >
+          Add expense
           </Button>
-          <Button onPress={this.clearAsyncStorage} >
-            Clear Data
+        <Button onPress={this.clearAsyncStorage} >
+          Clear Data
             </Button>
         <ScrollView>
 
-            <Text>Name:{items.name} Amount:{items.amount} Month:{items.month} Day:{items.day}</Text>
-            
+          {this.rendernameoftrans ()}
+          
 
 
-          </ScrollView>
-          <View style={{ flex: 1 }} />
-        </View>
-        
+
+        </ScrollView>
+        <View style={{ flex: 1 }} />
+      </View>
+
       //</PaperProvider>
     );
   }
